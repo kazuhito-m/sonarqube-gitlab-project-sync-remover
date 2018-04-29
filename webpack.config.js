75f1ac79032649c5f834276f0222f7aa62eb93e2
kazuhito-m <kazuhito.sumpic@gmail.com>
2018-04-29 23:12:44 +0900

    実装。TypeScriptでビルドできるまで。

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,20 @@
 module.exports = {
   mode: 'production',
-  entry: __dirname + "/src/main/ts/index.js", //ビルドするファイル
+  entry: __dirname + "/src/main/ts/index.ts", //ビルドするファイル
   output: {
-    path: __dirname +'/dist', //ビルドしたファイルを吐き出す場所
+    path: __dirname + '/dist', //ビルドしたファイルを吐き出す場所
     filename: 'bundle.js' //ビルドした後のファイル名
+  },
+  devtool: 'inline-source-map',
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [{
+      test: /\.ts?$/,
+      use: 'ts-loader',
+      exclude: /node_modules/
+    }]
   }
-};
+}
