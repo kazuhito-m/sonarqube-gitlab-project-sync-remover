8d20ff1d93f1d038983c210a91ae4c6be25968ce
kazuhito-m <kazuhito.sumpic@gmail.com>
2018-06-04 00:16:25 +0900

    構成変更。SourceMapを有効にするように

--- src/main/ts/index.ts
+++ src/main/ts/index.ts
@@ -1,4 +1,9 @@
 import ConsoleApplication from "./ConsoleApplication";
 process.on('unhandledRejection', console.dir);
+
+// Support source map.
+require('source-map-support').install()
+process.on('unhandledRejection', console.log);
+
 const app = new ConsoleApplication();
 app.run(process.argv);
