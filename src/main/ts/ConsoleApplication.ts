34c249772adedf11b06d5977ff04872bb06cbec0
kazuhito-m <kazuhito.sumpic@gmail.com>
2018-06-03 22:15:21 +0900

    fixtypo.

--- src/main/ts/ConsoleApplication.ts
+++ src/main/ts/ConsoleApplication.ts
@@ -2,7 +2,7 @@ import axiosBase from "axios";
 import Parameters from "./infrastracture/datasource/config/Parameters";
 import SynchronizeRemoveService from "./application/maintenance/SynchronizeRemoveService";
 
-export default class ConoleApplication {
+export default class ConsoleApplication {
   public async run(argv: string[]) {
     const parameters = new Parameters(argv);
     parameters.analyzeArgs();
