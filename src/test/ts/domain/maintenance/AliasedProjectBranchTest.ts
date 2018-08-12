51ebd105b8813c82b76a64f82e6880ab63d6ee0a
kazuhito-m <kazuhito.sumpic@gmail.com>
2018-08-12 23:46:49 +0900

    テスト修正。本体は直したが、テストが追随していなかったので修正。

--- src/test/ts/domain/maintenance/AliasedProjectBranchTest.ts
+++ src/test/ts/domain/maintenance/AliasedProjectBranchTest.ts
@@ -1,11 +1,12 @@
 import ProjectAndBranch from "../../../../main/ts/domain/maintenance/ProjectAndBranch";
 import AliasedProjectBranch from "../../../../main/ts/domain/maintenance/AliasedProjectBranch";
+import Aliases from '../../../../main/ts/domain/config/Aliases';
 jest.dontMock("../../../../main/ts/domain/maintenance/AliasedProjectBranch");
 
-const aliases: { [key: string]: string } = {
+const aliases: Aliases = new Aliases({
   元の名前: "置き換えた名前",
   べつの名前: ""
-};
+});
 
 describe("AliasedProjectBranch.projectName のテスト", () => {
   it("プロジェクト名がエイリアス辞書の中にあれば置き換える", () => {
